cfda1bd9f2f659018275825940d7054781fbbf1e
Ariel Fernando <arielaluno@gmail.com>
2020-06-04 16:54:19 -0300

    intial commit

---
+++ scripts/intro.js
@@ -0,0 +1,66 @@
+// comments
+// document.write("Hello")
+
+// //variaveis, tipos de dados
+// var myvar// underfined
+
+// myvar = "Hello"
+// const myconst = "He"
+
+// document.write(myconst + myvar)
+
+//string
+// const s1 = "Isso é uma string"
+// const s2 = 'Isso é uma string'
+// const s3 = `Isso é uma string`
+
+// document.write(s2)
+
+//number
+// const n1 = 1
+// const n2 = 12
+// document.write(n1 + n2);
+
+//boolean - true ou false
+// const bTrue = true
+// const bFalse = false
+// document.write(false)
+
+//Object possuem propriedades e funcionalidades
+
+// const pessoa = {
+//   altura: "1,80",
+//   idade: 24,
+//   solteiro: true,
+//   run(){
+//     document.write("executar uma grande logica de correr")
+//   }
+// }
+
+// pessoa.run()
+
+// Array = vetores
+
+// const colecaoDeBolinha = ["blue", "green", 1, {name: "my name"}]
+
+// document.write(colecaoDeBolinha[3].name)
+
+// function sayMyName(name){
+//   document.write(name)
+// };
+
+// sayMyName("Douglas")
+
+// Condicionais
+
+// const notafinal = 7 
+
+// if(notafinal < 5){
+//   document.write("Aprovado")
+// }else{
+//   document.write("reprovado");
+// }
+
+// for (i=0; i< 10; i++){
+//   document.write(`<p>${i}</p>`)
+// }
